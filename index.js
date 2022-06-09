68cc28de69789ce92cde0ca7a0288234f92fd7d4
Wandia E Mugo <wwandia173@gmail.com>
2022-06-09 20:57:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arrayName,eventName){
+    let thankYou= [];
+    for (let i=0; i<arrayName.length;i++) {
+        thankYou.push(`Thank you, ${arrayName[i]}, for the wonderful ${eventName} gift!`)
+    }  return thankYou;
+}
+
+function countDown(number){
+    while (number>0){
+        console.log(number);
+        number--;
+    }  console.log(number);
+} 
